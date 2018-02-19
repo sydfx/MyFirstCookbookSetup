f3d4fce2e1ac0d32b2608149bd9bc2fc8772ec7a
D <36378580+sydfx043@users.noreply.github.com>
2018-02-18 21:55:04 -0500

    v3.1

--- cookbooks/learn_chef_apache2/metadata.rb
+++ cookbooks/learn_chef_apache2/metadata.rb
@@ -1,20 +1,9 @@
-name 'learn_chef_apache2'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures learn_chef_apache2'
-long_description 'Installs/Configures learn_chef_apache2'
-version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/learn_chef_apache2/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/learn_chef_apache2'
+name 'learn_chef_httpd'
+maintainer 'David Guevremont'
+maintainer_email 'admin@mtlinfographie.com'
+license 'all_rights'
+description 'Installs/Configures learn_chef_httpd'
+long_description 'Installs/Configures learn_chef_httpd with user webmaster that can only write to the www directory'
+version '0.3.1'
+issues_url 'https://github.com/learn-chef/learn_chef_httpd/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/learn_chef_httpd' if respond_to?(:source_url)

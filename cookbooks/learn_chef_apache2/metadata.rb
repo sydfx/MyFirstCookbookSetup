2359b6551e3fb9b487d70a8b5abb864dfa6c3890
sydfx043 <info@mtlinfographie.com>
2018-02-19 21:59:43 -0500

    apache2/cookbook

--- cookbooks/learn_chef_apache2/metadata.rb
+++ cookbooks/learn_chef_apache2/metadata.rb
@@ -1,9 +1,9 @@
-name 'learn_chef_httpd'
+name 'learn_chef_apache2'
 maintainer 'David Guevremont'
 maintainer_email 'admin@mtlinfographie.com'
 license 'all_rights'
-description 'Installs/Configures learn_chef_httpd'
+description 'Installs/Configures learn_chef_apache2'
 long_description 'Installs/Configures learn_chef_httpd with user webmaster that can only write to the www directory'
-version '0.3.1'
-issues_url 'https://github.com/learn-chef/learn_chef_httpd/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/learn-chef/learn_chef_httpd' if respond_to?(:source_url)
+version '0.3.2'
+issues_url 'https://github.com/learn-chef/learn_chef_apache2/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/learn_chef_apache2' if respond_to?(:source_url)
